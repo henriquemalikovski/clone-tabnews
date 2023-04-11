86cb41b8349cc470aeb07f81fd8c3ec2aea873d7
Henrique Malikovski <hmalikovski@gmail.com>
2023-04-11 00:06:15 +0000

    Ajuste mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Teste</h1>
+  return <h1>O errado é errado mesmo que todos estejam fazendo, e o certo é certo mesmo que ninguém esteja fazendo.</h1>
 }
 
 export default Home
