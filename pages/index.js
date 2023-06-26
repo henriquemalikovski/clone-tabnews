5d44bbc8df6b6be3161de9a7bf890034ca898a0b
Henrique Malikovski <hmalikovski@gmail.com>
2023-06-26 13:38:27 +0000

    Adiciona scripts  `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>O errado é errado mesmo que todos estejam fazendo, e o certo é certo mesmo que ninguém esteja fazendo.</h1>
+  return (
+    <h1>
+      O errado é errado mesmo que todos estejam fazendo, e o certo é certo mesmo
+      que ninguém esteja fazendo.
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
