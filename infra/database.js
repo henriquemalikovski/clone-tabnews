9fa3e288d519ed834a26efe0de363f23fa52264d
Henrique Malikovski <hmalikovski@gmail.com>
2023-11-21 11:26:00 -0300

    Ajustes nos arquivos para userem o `.env`

--- infra/database.js
+++ infra/database.js
@@ -1,19 +1,19 @@
-import { Client } from "pg"
+import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD
-  })
-  await client.connect()
-  const result = await client.query(queryObject)
-  await client.end()
-  return result
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
 }
 
 export default {
   query: query
-}
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,7 @@ import database from "../../../../infra/database.js"
 
 async function status(req, res) {
   const result = await database.query('SELECT 1 + 1 AS sum')
-  console.log(result.rows)
+  // console.log(result.rows)
   res.status(200).json({ status: 'OK' })
 }
 
