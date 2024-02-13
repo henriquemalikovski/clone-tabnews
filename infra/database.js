7f6c0e2009999742327e2ef5cfaed4d368a0edf7
Henrique Malikovski <hmalikovski@gmail.com>
2024-02-13 20:44:36 -0300

    Refactor code and update dependencies

--- infra/database.js
+++ infra/database.js
@@ -1,25 +1,33 @@
-import { Client } from "pg";
+import { Client } from 'pg';
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  try {
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-  finally {
-    await client.end();
-  }
+	const client = new Client({
+		host: process.env.POSTGRES_HOST,
+		port: process.env.POSTGRES_PORT,
+		user: process.env.POSTGRES_USER,
+		database: process.env.POSTGRES_DB,
+		password: process.env.POSTGRES_PASSWORD,
+	});
+	console.log('Credentials:', {
+		host: process.env.POSTGRES_HOST,
+		port: process.env.POSTGRES_PORT,
+		user: process.env.POSTGRES_USER,
+		database: process.env.POSTGRES_DB,
+		password: process.env.POSTGRES_PASSWORD,
+	});
+
+	try {
+		await client.connect();
+		const result = await client.query(queryObject);
+		return result;
+	} catch (error) {
+		console.log(error);
+		throw error;
+	} finally {
+		await client.end();
+	}
 }
 
 export default {
-  query: query
+	query: query,
 };
