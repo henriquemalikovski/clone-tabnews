93cfb679a47f6897676a05968937c4c70e5acd20
Henrique Malikovski <hmalikovski@gmail.com>
2024-10-03 18:37:58 -0300

    makes `npm teste` more robust with `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,28 +1,25 @@
 import database from "infra/database.js";
+import orchestrator from "tests/orchestrator";
 
-beforeAll(clearDatabase)
-
-async function clearDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
-
+});
 
 test("POST to /api/v1/migrations should returns 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST"
-  })
+    method: "POST",
+  });
   const response1Body = await response1.json();
-  expect(response1.status).toBe(201)
+  expect(response1.status).toBe(201);
   expect(Array.isArray(response1Body)).toBe(true);
   expect(response1Body.length).toBeGreaterThan(0);
 
-
-
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST"
-  })
+    method: "POST",
+  });
   const response2Body = await response2.json();
-  expect(response2.status).toBe(200)
+  expect(response2.status).toBe(200);
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
-})
+});
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,12 +1,20 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
 test("GET to /api/v1/status should returns 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  expect(response.status).toBe(200)
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
 
-  const responseBody = await response.json()
-  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString()
-  expect(responseBody.updated_at).toBe(parseUpdatedAt)
+  const responseBody = await response.json();
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parseUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0")
-  expect(responseBody.dependencies.database.max_connections).toEqual(100)
-  expect(responseBody.dependencies.database.current_connections).toBeLessThanOrEqual(1);
-})
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(
+    responseBody.dependencies.database.current_connections
+  ).toBeLessThanOrEqual(1);
+});
