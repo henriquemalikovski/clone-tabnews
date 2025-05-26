c274d50b39272ed3fb33891116048bd5b4f00c88
Henrique Malikovski <hmalikovski@gmail.com>
2025-05-26 14:44:25 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

---
+++ infra/errors.js
@@ -0,0 +1,17 @@
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("Um erro interno não esperado ocorreu.", { cause });
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte técnico.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      statusCode: this.statusCode,
+    };
+  }
+}
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,30 +1,44 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(req, res) {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  const queryVersion = await database.query("SHOW server_version;");
-  const version = queryVersion.rows[0].server_version;
+    const queryVersion = await database.query("SHOW server_version;");
+    const version = queryVersion.rows[0].server_version;
 
-  const queryMaxConnections = await database.query("SHOW max_connections;");
-  const maxConnections = parseInt(queryMaxConnections.rows[0].max_connections);
+    const queryMaxConnections = await database.query("SHOW max_connections;");
+    const maxConnections = parseInt(
+      queryMaxConnections.rows[0].max_connections,
+    );
 
-  const databaseName = process.env.POSTGRES_DB;
-  const queryCurrentConnections = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  const currentConnections = queryCurrentConnections.rows[0].count;
-  res.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: version,
-        max_connections: maxConnections,
-        current_connections: currentConnections,
+    const databaseName = process.env.POSTGRES_DB;
+    const queryCurrentConnections = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+    const currentConnections = queryCurrentConnections.rows[0].count;
+    res.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: version,
+          max_connections: maxConnections,
+          current_connections: currentConnections,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\n Erro dentro do catch do controller");
+    console.log(publicErrorObject);
+
+    res.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
