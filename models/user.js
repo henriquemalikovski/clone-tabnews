06ad6a1c24a7b1ab1241edc8eec2a11d79e0a54b
Henrique Malikovski <hmalikovski@gmail.com>
2025-05-27 21:31:40 -0300

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,36 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
+
+async function findOneUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+          SELECT
+            *
+          FROM
+            users
+          WHERE
+            LOWER(username) = LOWER($1)
+          LIMIT 
+            1
+          ;`,
+      values: [username],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        name: "NotFoundError",
+        message: "The username provided was not found.",
+        action: "Please check the username and try again.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -28,6 +59,7 @@ async function create(userInputValues) {
       });
     }
   }
+
   async function validateUniqueUsername(username) {
     const result = await database.query({
       text: `
@@ -71,6 +103,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneUsername,
 };
 
 export default user;
